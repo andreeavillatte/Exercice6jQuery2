5d060e2c6da2e3f6f288f3d34cea3ec5a83ebc9f
core.editor <andreeavillatte@gmail.com>
2019-04-19 11:20:10 +0200

    faciliter avec ajout event .HOVER

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,31 @@
 // //quand la page est chargée (ready)
 $(document).ready(function () {
   // quand la souris passe sur une couleur je demande de choisir la class color
-  $('.color').mouseenter(function () {
+  $('.color').hover(function () {
     //je récupère la deuxième classe de l'élément sur lequel j'ai cliqué
     var color = $(this).attr('class').split(' ')[1];
     // je change le text en fonction de la couleur sur la quelle j'ai appuié
     $('#text').css('color', color);
-  });
-  // quand la souris quitte le bouton je demande que le texte revienne noir en utilisant l'evenement .mouseleave
-  $('.color').mouseleave(function () {
+  },
     // je change le text en noir en utilisant le css
-    $('#text').css('color', 'black');
-  });
+    function () {
+      $('#text').css('color', 'black');
+    });
 });
+
+//  Une methode qui functione très bien, mais la prémiere méthode est plus courte 
+// // //quand la page est chargée (ready)
+// $(document).ready(function () {
+//   // quand la souris passe sur une couleur je demande de choisir la class color
+//   $('.color').mouseenter(function () {
+//     //je récupère la deuxième classe de l'élément sur lequel j'ai cliqué
+//     var color = $(this).attr('class').split(' ')[1];
+//     // je change le text en fonction de la couleur sur la quelle j'ai appuié
+//     $('#text').css('color', color);
+//   });
+//   // quand la souris quitte le bouton je demande que le texte revienne noir en utilisant l'evenement .mouseleave
+//   $('.color').mouseleave(function () {
+//     // je change le text en noir en utilisant le css
+//     $('#text').css('color', 'black');
+//   });
+// });
